24af3c03e261c5982bc79cb0db857341d7ea98c1
dafe2201 <daniel_fellden@hotmail.com>
2022-11-22 14:08:51 +0100

    added Class game

---
+++ src/ServerSide/Game.java
@@ -0,0 +1,29 @@
+package ServerSide;
+
+import java.io.ObjectOutputStream;
+
+public class Game {
+        DAO dao = new DAO();
+        Protocol playerOne, PlayerTwo;
+        Protocol currentPlayer;
+
+        int currentRound;
+        int scoreCounter;
+
+        public void setCurrentRound(){
+
+        }
+
+        public void getPlayerScore(){
+
+        }
+
+        public void setTotalResult(){
+
+        }
+
+        public void generateWinScreenData(){
+
+        }
+
+}
