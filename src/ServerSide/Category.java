481d10ba147c39c924f0d1a23f4e38ce92fc969e
dafe2201 <daniel_fellden@hotmail.com>
2022-11-23 11:31:18 +0100

    Added ChooseCategoryScreen base.

---
+++ src/ServerSide/Category.java
@@ -0,0 +1,32 @@
+package ServerSide;
+
+import Resources.QuestionsWithAnswers;
+
+import java.util.List;
+
+public class Category {
+
+    String name;
+    List<QuestionsWithAnswers> questionPackage;
+
+    public Category(List<QuestionsWithAnswers> questionPackage, String name){
+        this.questionPackage = questionPackage;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<QuestionsWithAnswers> getQuestionPackage() {
+        return questionPackage;
+    }
+
+    public void setQuestionPackage(List<QuestionsWithAnswers> questionPackage) {
+        this.questionPackage = questionPackage;
+    }
+}
